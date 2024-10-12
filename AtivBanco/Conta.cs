00e2fe7db79648bb4c39cd84d0a40f1c0c7af395
Lucas da Silva Nunes <lucas.silvaifro@gmail.com>
2024-10-11 20:40:01 -0400

    Inicio da ativade

---
+++ AtivBanco/Conta.cs
@@ -0,0 +1,33 @@
+﻿public class Conta
+{
+    public int numConta;
+    public string agencia;
+    public string nome;
+    public double saldo;
+
+    public void Depositar(double valorDepo)
+    {
+        if (valorDepo < 0)
+        {
+            Console.WriteLine("valor negativo");
+        }
+        else
+        {
+            saldo = saldo + valorDepo;
+        }
+    }
+
+    public void Sacar(double valorSacar)
+    {
+        if (valorSacar > saldo || valorSacar <=0)
+        {
+            Console.WriteLine("saldo insuficiente ou valor inválido");
+        }
+        else
+        {
+            Console.WriteLine("Saldo suficiente");
+            saldo = saldo - valorSacar;
+            //Console.WriteLine($"Saldo da Conta {valorSacar}");
+        }
+    }
+}
---
+++ AtivBanco/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+Conta cont =  new Conta();
+
+Console.WriteLine("Digite seu nome: ");
+cont.nome = Console.ReadLine();
+
+
+Console.WriteLine("Digite sua Agência");
+cont.agencia = Console.ReadLine();
+Console.WriteLine("Digite o numero da sua conta: ");
+cont.numConta = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite seu saldo em conta: ");
+cont.saldo = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("digite D para Depositar");
+Console.WriteLine("Digite S para Sacar");
+string op = Console.ReadLine();
+
+switch (op)
+{
+	case "D":
+        Console.WriteLine("Digite o valor de Deposito");
+		cont.Depositar(Convert.ToDouble(Console.ReadLine()));
+
+        Console.WriteLine($"Saldo de {cont.saldo}");
+
+		break;
+	case "S":
+		Console.WriteLine("digite o valor de Saque");
+		cont.Sacar(Convert.ToDouble(Console.ReadLine()));
+
+		Console.WriteLine($"Saldo de {cont.saldo}");
+
+
+        break;
+	default:
+		break;
+}
